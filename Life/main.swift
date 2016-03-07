514cf70142de36cedb9144f0ab2d853dd512ca89
dwestgate <dwestgate@usfca.edu>
2016-03-07 18:24:01 -0500

    Initial Commit

---
+++ Life/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Life
+//
+//  Created by David Westgate on 3/7/16.
+//  Copyright © 2016 Refabricants. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
